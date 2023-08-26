05c4e01de800d418b9934b9c681092687f927d69
Tomás Marmoni <tdm1094@gmail.com>
2023-08-26 10:45:18 -0300

    refactor: Modificados nombres de métodos para no usar camelcase

--- app/lista_de_tareas.py
+++ app/lista_de_tareas.py
@@ -1,37 +1,30 @@
-# Imports
-import datetime
 from tarea import Tarea
 
-class ListaDeTareas:
 
+class ListaDeTareas:
     def __init__(self, nombre):
         self.nombre = nombre
         self.tareas = []
 
-    def agregarTareaNueva(self, nombre, descripcion, fecha_vencimiento):
+    def agregar_tarea_nueva(self, nombre, descripcion, fecha_vencimiento):
         tarea = Tarea(nombre, descripcion, fecha_vencimiento)
         self.tareas.append(tarea)
 
-    def eliminarTarea(self, tarea):
+    def eliminar_tarea(self, tarea):
         self.tareas.remove(tarea)
 
-    def modificarNombreTarea(self, tarea, nuevo_nombre): # La tarea estará expresada con el índice +1
+    def modificar_tarea_nombre(self, tarea, nuevo_nombre):  # La tarea estará expresada con el índice +1
         tarea.nombre = nuevo_nombre
 
-    def modificarDescripcionTarea(self, tarea, nueva_descripcion):
+    def modificar_tarea_descripcion(self, tarea, nueva_descripcion):
         tarea.descripcion = nueva_descripcion
 
-    def agregarFechaVencimientoATarea(self, tarea, nueva_fecha_vencimiento):
+    def agregar_tarea_nueva_fecha_vencimiento(self, tarea, nueva_fecha_vencimiento):
         tarea.fecha_vencimiento = nueva_fecha_vencimiento
 
-    def quitarFechaVencimientoDeTarea(self, tarea):
+    def eliminar_tarea_fecha_vencimiento(self, tarea):
         del tarea.fecha_vencimiento
 
-        if not self.tareas:
-            return "No hay tareas en esta lista."
-        else:
-            return print(self.tareas)
-    
     def __str__(self):
         if not self.tareas:
             print("No hay tareas en esta lista.")
