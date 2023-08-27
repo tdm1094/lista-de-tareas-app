0f3dc95fef44a44bf00f64f7236ab709a2af9d84
Tomás Marmoni <tdm1094@gmail.com>
2023-08-27 13:48:30 -0300

    refactor(lista_de_tareas): Cambiadas firmas de métodos

--- app/lista_de_tareas.py
+++ app/lista_de_tareas.py
@@ -1,29 +1,36 @@
-from tarea import Tarea
-
-
 class ListaDeTareas:
     def __init__(self, nombre):
         self.nombre = nombre
         self.tareas = []
 
-    def agregar_tarea_nueva(self, nombre, descripcion, fecha_vencimiento):
-        tarea = Tarea(nombre, descripcion, fecha_vencimiento)
-        self.tareas.append(tarea)
+    def __str__(self):
+        if not self.tareas:
+            print("No hay tareas en esta lista.")
+        else:
+            print("Tareas:")
+            for i, tarea in enumerate(self.tareas, 1):
+                print(f"{i}. {tarea}")
 
-    def eliminar_tarea(self, tarea):
-        self.tareas.remove(tarea)
+    def agregar_tarea_nueva(self, nueva_tarea):
+        self.tareas.append(nueva_tarea)
 
-    def modificar_tarea_nombre(self, tarea, nuevo_nombre):  # La tarea estará expresada con el índice +1
-        tarea.nombre = nuevo_nombre
+    def eliminar_tarea(self, indice_tarea):
+        del self.tareas[indice_tarea]
 
-    def modificar_tarea_descripcion(self, tarea, nueva_descripcion):
-        tarea.descripcion = nueva_descripcion
+    def modificar_tarea_nombre(self, indice_tarea, nuevo_nombre):
+        self.tareas[indice_tarea].nombre = nuevo_nombre
+
+    def modificar_tarea_descripcion(self, indice_tarea, nueva_descripcion):
+        self.tareas[indice_tarea].descripcion = nueva_descripcion
 
     def agregar_o_modificar_tarea_fecha_vencimiento(self, indice_tarea, nueva_fecha_vencimiento):
         self.tareas[indice_tarea].fecha_vencimiento = nueva_fecha_vencimiento
 
-    def eliminar_tarea_fecha_vencimiento(self, tarea):
-        del tarea.fecha_vencimiento
+    def eliminar_tarea_fecha_vencimiento(self, indice_tarea):
+        self.tareas[indice_tarea].fecha_vencimiento = None
+
+    def cambiar_estado_tarea(self, indice_tarea):
+        self.tareas[indice_tarea].cambiar_estado()
 
     def __str__(self):
         if not self.tareas:
