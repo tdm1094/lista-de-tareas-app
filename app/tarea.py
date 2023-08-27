be07369791ae07e2416cc95e65adb6ed7141c480
Tomás Marmoni <tdm1094@gmail.com>
2023-08-27 13:51:23 -0300

    feat(Tarea): Agregados métodos para cambiar estado y consultar si está vencida

--- app/tarea.py
+++ app/tarea.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Tarea:
     def __init__(self, nombre, descripcion=None, fecha_vencimiento=None):
         self.nombre = nombre
@@ -10,3 +13,9 @@ class Tarea:
                f"Descripción: {self.descripcion}\n" \
                f"Fecha de vencimiento: {self.fecha_vencimiento}\n" \
                f"Estado: {self.estado}"
+
+    def cambiar_estado(self):
+        self.estado = not self.estado
+
+    def esta_vencida(self):
+        return self.fecha_vencimiento < datetime.datetime.now()
